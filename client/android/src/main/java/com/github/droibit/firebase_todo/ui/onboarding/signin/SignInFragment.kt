2ebe73ecd250a833264f64efa16e368e8bf0843a
Shinya Kumagai <roomful.rooms@gmail.com>
2021-01-22 00:35:14 +0900

    WIP: android: Sign in with Google

--- client/android/src/main/java/com/github/droibit/firebase_todo/ui/onboarding/signin/SignInFragment.kt
+++ client/android/src/main/java/com/github/droibit/firebase_todo/ui/onboarding/signin/SignInFragment.kt
@@ -1,15 +1,40 @@
 package com.github.droibit.firebase_todo.ui.onboarding.signin
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.fragment.app.Fragment
+import com.github.aakira.napier.Napier
 import com.github.droibit.firebase_todo.databinding.FragmentSignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.ApiException
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SignInFragment : Fragment() {
+
+    @Inject
+    lateinit var googleSignInClient: GoogleSignInClient
+
+    private val signInWithGoogle = registerForActivityResult(StartActivityForResult()) {
+        if (it.resultCode != Activity.RESULT_OK || it.data == null) {
+            return@registerForActivityResult
+        }
+
+        try {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            // Google Sign In was successful, authenticate with Firebase
+            val account = requireNotNull(task.getResult(ApiException::class.java))
+            Napier.d("firebaseAuthWithGoogle: ${account.idToken}")
+        } catch (e: ApiException) {
+            Napier.e("Google sign in failed", e)
+        }
+    }
 
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = requireNotNull(_binding)
@@ -24,6 +49,14 @@ class SignInFragment : Fragment() {
                 it.lifecycleOwner = viewLifecycleOwner
                 this._binding = it
             }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signInButton.setOnClickListener {
+            signInWithGoogle.launch(googleSignInClient.signInIntent)
+        }
     }
 
     override fun onDestroyView() {
---
+++ client/android/src/main/java/com/github/droibit/firebase_todo/ui/onboarding/signin/SignInModule.kt
@@ -0,0 +1,26 @@
+package com.github.droibit.firebase_todo.ui.onboarding.signin
+
+import android.content.Context
+import com.github.droibit.firebase_todo.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(FragmentComponent::class)
+object SignInModule {
+
+    @Provides
+    fun provideGoogleSignInClient(@ApplicationContext context: Context): GoogleSignInClient {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(context, gso)
+    }
+}
