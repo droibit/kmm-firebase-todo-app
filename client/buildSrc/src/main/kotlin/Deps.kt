04ca088d0631518d09402b814c188ba2f2f91a4e
Shinya Kumagai <roomful.rooms@gmail.com>
2021-02-27 20:05:59 +0900

    [android] Tweak dark theme

--- client/buildSrc/src/main/kotlin/Deps.kt
+++ client/buildSrc/src/main/kotlin/Deps.kt
@@ -90,7 +90,7 @@ object Deps {
     }
 
     const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-core:1.1.0"
-    const val materialDesign = "com.google.android.material:material:1.3.0"
+    const val materialDesign = "com.google.android.material:material:1.4.0-alpha01"
     const val coil = "io.coil-kt:coil:1.1.1"
     const val napier = "com.github.aakira:napier:1.4.1"
     const val inject = "com.chrynan.inject:inject-common:0.7.0"
