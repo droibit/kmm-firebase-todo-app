c9f3efeea8110d93e9be408a8a9ed2e7817f2ebc
Shinya Kumagai <roomful.rooms@gmail.com>
2021-03-19 18:37:33 +0900

    [firebase] WIP: Update firebase.test.ts

--- firebase/firestore/__tests__/firestore.test.ts
+++ firebase/firestore/__tests__/firestore.test.ts
@@ -1,3 +1,60 @@
+import * as firebase from "@firebase/rules-unit-testing";
+import * as fs from "fs";
+import * as http from "http";
+
+// ref. https://github.com/firebase/quickstart-testing/blob/master/unit-test-security-rules/test/firestore.spec.js
+
+/**
+ * The emulator will accept any project ID for testing.
+ */
+const PROJECT_ID = "firebase-todo";
+
+/**
+ * The FIRESTORE_EMULATOR_HOST environment variable is set automatically
+ * by "firebase emulators:exec"
+ */
+const COVERAGE_URL = `http://${process.env.FIRESTORE_EMULATOR_HOST}/emulator/v1/projects/${PROJECT_ID}:ruleCoverage.html`;
+
 test("Hello, jest", async () => {
   expect(1).toBe(1);
-})
+});
+
+/**
+ * Creates a new client FirebaseApp with authentication and returns the Firestore instance.
+ */
+function getAuthedFirestore(auth: any) {
+  return firebase
+    .initializeTestApp({ projectId: PROJECT_ID, auth })
+    .firestore();
+}
+
+beforeAll(async () => {
+  // Load the rules file before the tests begin
+  const rules = fs.readFileSync("firestore.rules", "utf8");
+  await firebase.loadFirestoreRules({ projectId: PROJECT_ID, rules });
+});
+
+beforeEach(async () => {
+  // Clear the database between tests
+  await firebase.clearFirestoreData({ projectId: PROJECT_ID });
+});
+
+afterAll(async () => {
+  // Delete all the FirebaseApp instances created during testing
+  // Note: this does not affect or clear any data
+  await Promise.all(firebase.apps().map((app) => app.delete()));
+
+  // Write the coverage report to a file
+  const coverageFile = "firestore-coverage.html";
+  const fstream = fs.createWriteStream(coverageFile);
+  await new Promise((resolve, reject) => {
+    http.get(COVERAGE_URL, (res) => {
+      res.pipe(fstream, { end: true });
+
+      res.on("end", resolve);
+      res.on("error", reject);
+    });
+  });
+
+  console.log(`View firestore rule coverage information at ${coverageFile}\n`);
+});
