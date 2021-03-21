1737d88c4177db299722484d04b9dee60756c042
Shinya Kumagai <roomful.rooms@gmail.com>
2021-03-22 02:35:22 +0900

    [firebase] WIP:  Add a background function for Firebase user creation

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,4 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
---
+++ firebase/functions/src/user.ts
@@ -0,0 +1,23 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { Statistics, User } from "./model";
+
+export const onAuthUserCreate = functions.auth.user().onCreate(async (user) => {
+  const batch = admin.firestore().batch();
+
+  const newUser: User = {
+    name: user.displayName!,
+    photoURL: user.photoURL,
+  };
+  const newUserRef = admin.firestore().doc(`users/${user.uid}`);
+  newUserRef.set(newUser);
+
+  const initialStatistics: Statistics = {
+    numberOfActiveTasks: 0,
+    numberOfCompletedTask: 0,
+    updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+  };
+  newUserRef.collection("statistics").doc("task").set(initialStatistics);
+
+  batch.commit();
+});
