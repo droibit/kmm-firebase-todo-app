fd39563684e7377720cc6ee811c32205b3fea66b
Shinya Kumagai <roomful.rooms@gmail.com>
2021-03-29 15:07:09 +0900

    [android] Update some dependencies

--- client/buildSrc/src/main/kotlin/Deps.kt
+++ client/buildSrc/src/main/kotlin/Deps.kt
@@ -5,7 +5,7 @@ object Deps {
             const val gradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
             const val serialization = "org.jetbrains.kotlin:kotlin-serialization:$version"
         }
-        const val android = "com.android.tools.build:gradle:4.2.0-beta05"
+        const val android = "com.android.tools.build:gradle:4.2.0-beta06"
         const val googleServices = "com.google.gms:google-services:4.3.4"
         const val daggerHilt = "com.google.dagger:hilt-android-gradle-plugin:${Dagger.version}"
         const val navSafeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Androidx.Navigation.version}"
@@ -18,7 +18,7 @@ object Deps {
 
     object Coroutines {
         // Strictly requires `native-mt` version.
-        private const val version = "1.4.2-native-mt!!"
+        private const val version = "1.4.3-native-mt!!"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${version}"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
         const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
@@ -29,24 +29,24 @@ object Deps {
     }
 
     object Androidx {
-        const val core = "androidx.core:core-ktx:1.5.0-beta02"
-        const val activity = "androidx.activity:activity-ktx:1.2.0"
-        const val fragment = "androidx.fragment:fragment-ktx:1.3.0"
-        const val appCompat = "androidx.appcompat:appcompat:1.3.0-beta01"
+        const val core = "androidx.core:core-ktx:1.6.0-alpha01"
+        const val activity = "androidx.activity:activity-ktx:1.3.0-alpha05"
+        const val fragment = "androidx.fragment:fragment-ktx:1.3.2"
+        const val appCompat = "androidx.appcompat:appcompat:1.3.0-rc01"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
-        const val recyclerView = "androidx.recyclerview:recyclerview:1.2.0-beta01"
-        const val dataStore = "androidx.datastore:datastore-preferences:1.0.0-alpha07"
+        const val recyclerView = "androidx.recyclerview:recyclerview:1.2.0-rc01"
+        const val dataStore = "androidx.datastore:datastore-preferences:1.0.0-alpha08"
         const val preference = "androidx.preference:preference-ktx:1.1.1"
 
         object Navigation {
-            internal const val version = "2.3.3"
+            internal const val version = "2.3.4"
             const val runtime = "androidx.navigation:navigation-runtime-ktx:$version"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val ui = "androidx.navigation:navigation-ui-ktx:$version"
         }
 
         object Lifecycle {
-            private const val version = "2.3.0"
+            private const val version = "2.4.0-alpha01"
             const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
             const val viewModel = "androidx.navigation:navigation-fragment-ktx:$version"
         }
@@ -67,7 +67,7 @@ object Deps {
             const val functions = "dev.gitlive:firebase-functions:$version"
         }
 
-        const val analytics = "com.google.firebase:firebase-analytics-ktx:17.3.0"
+        const val analytics = "com.google.firebase:firebase-analytics-ktx:18.0.2"
     }
 
     object PlayServices {
@@ -75,7 +75,7 @@ object Deps {
     }
 
     object Settings {
-        private const val version = "0.7.3"
+        private const val version = "0.7.4"
         const val core = "com.russhwolf:multiplatform-settings:$version"
         const val coroutines = "com.russhwolf:multiplatform-settings-coroutines-native-mt:$version"
         const val datastore = "com.russhwolf:multiplatform-settings-datastore:$version"
