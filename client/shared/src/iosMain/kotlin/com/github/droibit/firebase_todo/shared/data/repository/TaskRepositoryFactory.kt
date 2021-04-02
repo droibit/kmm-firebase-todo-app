de3bfbae8e6dfc5db2cd2bb3e07081437e930eea
Shinya Kumagai <roomful.rooms@gmail.com>
2021-04-02 14:38:44 +0900

    [iOS] Fix signature of `TaskRepositoryFactory#make`

--- client/shared/src/iosMain/kotlin/com/github/droibit/firebase_todo/shared/data/repository/TaskRepositoryFactory.kt
+++ client/shared/src/iosMain/kotlin/com/github/droibit/firebase_todo/shared/data/repository/TaskRepositoryFactory.kt
@@ -2,15 +2,24 @@ package com.github.droibit.firebase_todo.shared.data.repository
 
 import com.github.droibit.firebase_todo.shared.data.repository.task.TaskRepository
 import com.github.droibit.firebase_todo.shared.data.source.settings.UserSettingsDataSource
+import com.github.droibit.firebase_todo.shared.data.source.task.TaskDataSource
+import com.github.droibit.firebase_todo.shared.data.source.user.UserDataSource
 import com.github.droibit.firebase_todo.shared.utils.CoroutinesDispatcherProvider
+import kotlinx.coroutines.GlobalScope
 
 object TaskRepositoryFactory {
     fun make(
-        userSettingsDataSource: UserSettingsDataSource
+        userDataSource: UserDataSource,
+        taskDataSource: TaskDataSource,
+        userSettingsDataSource: UserSettingsDataSource,
     ): TaskRepository {
         return TaskRepository(
+            userDataSource,
+            taskDataSource,
             userSettingsDataSource,
-            CoroutinesDispatcherProvider.sharedInstance
+            CoroutinesDispatcherProvider.sharedInstance,
+            // TODO: Consider make a CoroutineScope for this application lifecycle.
+            externalScope = GlobalScope
         )
     }
 }
