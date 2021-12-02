ef594b887fb06ea1ed9b02740a8255e9b1d17822
Shinya Kumagai <roomful.rooms@gmail.com>
2021-12-02 18:08:15 +0900

    android: Support multiple backstacks with BottomNavgationView

--- client/android/src/main/java/com/github/droibit/firebase_todo/ui/main/MainActivity.kt
+++ client/android/src/main/java/com/github/droibit/firebase_todo/ui/main/MainActivity.kt
@@ -7,19 +7,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.github.droibit.firebase_todo.R
 import com.github.droibit.firebase_todo.databinding.ActivityMainBinding
-import com.github.droibit.firebase_todo.utils.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * ref.
- * - [NavigationAdvancedSample/MainActivity.kt](https://github.com/android/architecture-components-samples/blob/master/NavigationAdvancedSample/app/src/main/java/com/example/android/navigationadvancedsample/MainActivity.kt)
- * - [NavigationAdvancedSample/NavigationExtensions.kt](https://github.com/android/architecture-components-samples/blob/master/NavigationAdvancedSample/app/src/main/java/com/example/android/navigationadvancedsample/NavigationExtensions.kt)
- */
 @AndroidEntryPoint
 class MainActivity :
     AppCompatActivity(),
@@ -27,11 +22,12 @@ class MainActivity :
 
     private lateinit var binding: ActivityMainBinding
 
-    private var currentNavController: LiveData<NavController>? = null
+    private lateinit var navController: NavController
 
     private val isInDarkMode: Boolean
         get() {
-            val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            val currentNightMode =
+                resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
             return currentNightMode == Configuration.UI_MODE_NIGHT_YES
         }
 
@@ -40,36 +36,10 @@ class MainActivity :
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
 
-        if (savedInstanceState == null) {
-            setupBottomNavigationBar()
-        } // Else, need to wait for onRestoreInstanceState
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        // Now that BottomNavigationBar has restored its instance state
-        // and its selectedItemId, we can proceed with setting up the
-        // BottomNavigationBar with Navigation
-        setupBottomNavigationBar()
-    }
-
-    private fun setupBottomNavigationBar() {
-        val navGraphIds = listOf(R.navigation.tasks, R.navigation.statistics)
-
-        // Setup the bottom navigation view with a list of navigation graphs
-        val controller = binding.bottomNav.setupWithNavController(
-            navGraphIds = navGraphIds,
-            fragmentManager = supportFragmentManager,
-            containerId = R.id.navHostContainer,
-            intent = intent,
-            listener = this
-        )
-
-        // // Whenever the selected controller changes, setup the action bar.
-        // controller.observe(this) { navController ->
-        //     setupActionBarWithNavController(navController)
-        // }
-        currentNavController = controller
+        val navHostFragment = binding.navHostContainer.getFragment<NavHostFragment>()
+        navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener(this)
+        binding.bottomNav.setupWithNavController(navController)
 
         if (isInDarkMode) {
             // workaround for removing the elevation color overlay
@@ -79,7 +49,7 @@ class MainActivity :
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return currentNavController?.value?.navigateUp() ?: false
+        return navController.navigateUp()
     }
 
     override fun onDestinationChanged(
