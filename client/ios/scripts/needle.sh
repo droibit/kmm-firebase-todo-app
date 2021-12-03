cf7424ae3bc824127c7f2d51811b5db0ccfb5f25
Shinya Kumagai <roomful.rooms@gmail.com>
2021-12-03 14:38:35 +0900

    iOS: Support Xcode13

--- client/ios/scripts/needle.sh
+++ client/ios/scripts/needle.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ $ACTION == "indexbuild" ]; then
+  echo "Not running needle generator during indexing."
+  exit 0 
+fi
+
 export SOURCEKIT_LOGGING=0 && Carthage/Checkouts/needle/Generator/bin/needle generate TodoApp/DI/NeedleGenerated.swift TodoApp/
